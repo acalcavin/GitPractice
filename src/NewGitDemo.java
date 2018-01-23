395f70fc28c76dd6e1e08676e1adf97bcd2a95b0
aicavin <aicavin@gmail.com>
2018-01-23 14:20:58 -0500

    add project

---
+++ src/NewGitDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter first name:");
+		String firstName = scnr.next();
+
+		System.out.println("You entered: " + firstName);
+		
+	}
+
+}
